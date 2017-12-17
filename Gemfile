6e6354229612c51a0bf0792f283e845412d85905
biswasthapa <viswasthapa@gmail.com>
2017-12-17 14:31:07 +1100

    Ahoy Gem for analytics to track visits to website

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem 'devise'
 # file uploads
 gem "paperclip", '~> 5.0.0'
 
+# Ahoy for analytics
+
+gem 'ahoy_matey'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171217022317) do
+ActiveRecord::Schema.define(version: 20171217032415) do
+
+  create_table "ahoy_events", force: :cascade do |t|
+    t.integer  "visit_id",   limit: 4
+    t.integer  "user_id",    limit: 4
+    t.string   "name",       limit: 255
+    t.text     "properties", limit: 65535
+    t.datetime "time"
+  end
+
+  add_index "ahoy_events", ["name", "time"], name: "index_ahoy_events_on_name_and_time", using: :btree
+  add_index "ahoy_events", ["user_id", "name"], name: "index_ahoy_events_on_user_id_and_name", using: :btree
+  add_index "ahoy_events", ["visit_id", "name"], name: "index_ahoy_events_on_visit_id_and_name", using: :btree
 
   create_table "authors", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -67,6 +79,38 @@ ActiveRecord::Schema.define(version: 20171217022317) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "visits", force: :cascade do |t|
+    t.string   "visit_token",      limit: 255
+    t.string   "visitor_token",    limit: 255
+    t.string   "ip",               limit: 255
+    t.text     "user_agent",       limit: 65535
+    t.text     "referrer",         limit: 65535
+    t.text     "landing_page",     limit: 65535
+    t.integer  "user_id",          limit: 4
+    t.string   "referring_domain", limit: 255
+    t.string   "search_keyword",   limit: 255
+    t.string   "browser",          limit: 255
+    t.string   "os",               limit: 255
+    t.string   "device_type",      limit: 255
+    t.integer  "screen_height",    limit: 4
+    t.integer  "screen_width",     limit: 4
+    t.string   "country",          limit: 255
+    t.string   "region",           limit: 255
+    t.string   "city",             limit: 255
+    t.string   "postal_code",      limit: 255
+    t.decimal  "latitude",                       precision: 10
+    t.decimal  "longitude",                      precision: 10
+    t.string   "utm_source",       limit: 255
+    t.string   "utm_medium",       limit: 255
+    t.string   "utm_term",         limit: 255
+    t.string   "utm_content",      limit: 255
+    t.string   "utm_campaign",     limit: 255
+    t.datetime "started_at"
+  end
+
+  add_index "visits", ["user_id"], name: "index_visits_on_user_id", using: :btree
+  add_index "visits", ["visit_token"], name: "index_visits_on_visit_token", unique: true, using: :btree
 
   add_foreign_key "books", "authors"
   add_foreign_key "books", "categories"
