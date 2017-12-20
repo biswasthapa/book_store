932ce72e21609b6008556d20ead91ef37064763f
biswasthapa <viswasthapa@gmail.com>
2017-12-20 17:53:32 +1100

    Seeds, routes and images for initial book seed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :publishers
   resources :categories
   resources :authors
+  resources :visits, only: :index
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -58,4 +59,6 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root 'books#index'
 end
